18c51d1f9565feb12f54d70154b6532d10895969
TKYOpartner <128873901+TKYOpartner@users.noreply.github.com>
2023-10-28 15:27:30 -0400

    More styling done

--- redditapp/src/App.js
+++ redditapp/src/App.js
@@ -1,15 +1,17 @@
-// src/App.js
 import React from 'react';
 import PostList from './components/postList';
 import './styles/App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="app-container">
       <div className="app-title">ReddTok</div>  
       <div className="post-container"> 
         <PostList />
       </div>  
+      <footer className="app-footer">
+        © 2023 ReddTok
+      </footer>
     </div>
   );
 }
